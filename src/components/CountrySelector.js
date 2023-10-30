09299e288f2020d162e24986c1e796a5749213c2
Sara Elsayeda <swe@saraelsa.net>
2023-10-29 22:06:37 -0400

    Add onBlur event to CountrySelector

--- src/components/CountrySelector.js
+++ src/components/CountrySelector.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import React, {useEffect} from 'react';
 import {View} from 'react-native';
+import { useNavigationState } from '@react-navigation/native';
+import usePrevious from '@hooks/usePrevious';
 import useLocalize from '@hooks/useLocalize';
+import lodashGet from 'lodash/get';
 import Navigation from '@libs/Navigation/Navigation';
 import styles from '@styles/styles';
 import ROUTES from '@src/ROUTES';
@@ -14,6 +17,9 @@ const propTypes = {
 
     /** Callback called when the country changes. */
     onInputChange: PropTypes.func.isRequired,
+
+    /** Callback called when the selector is dismissed. */
+    onBlur: PropTypes.func,
 
     /** Current selected country  */
     value: PropTypes.string,
@@ -28,21 +34,34 @@ const propTypes = {
 
 const defaultProps = {
     errorText: '',
+    onBlur: undefined,
     value: undefined,
     forwardedRef: () => {},
 };
 
-function CountrySelector({errorText, value: countryCode, onInputChange, forwardedRef}) {
+function CountrySelector({errorText, value: countryCode, onInputChange, onBlur, forwardedRef}) {
     const {translate} = useLocalize();
 
     const title = countryCode ? translate(`allCountries.${countryCode}`) : '';
     const countryTitleDescStyle = title.length === 0 ? styles.textNormal : null;
+
+    const activeRouteName = useNavigationState(state => lodashGet(state, ['routes', state.index, 'name']));
+    const isFocused = activeRouteName === 'Settings_PersonalDetails_Address_Country';
+    const prevFocused = usePrevious(isFocused);
 
     useEffect(() => {
         // This will cause the form to revalidate and remove any error related to country name
         onInputChange(countryCode);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [countryCode]);
+
+    useEffect(() => {
+        // A blur is detected when focused changes from true to false.
+        if (prevFocused && isFocused === false && onBlur) {
+            onBlur();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isFocused, prevFocused]);
 
     return (
         <View>
